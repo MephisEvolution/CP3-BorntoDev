107acede2c7e68e7a7b3d6a2a53e96f10a1d1a20
MephisEvolution <mephis.evolution@gmail.com>
2026-09-21 21:44:26 +0700

    Create Exercise4_Grade_SL.py

---
+++ Assignments /Exercise4_Grade_SL.py
@@ -0,0 +1,9 @@
+FoundationEnglish = "A"
+GeneralBusiness = "B"
+IntroductiontoComputerScience = "A"
+ComputerProgramming="B"
+print("--- Your Score ---")
+print("Foundation English",":", FoundationEnglish)
+print("General Business",":", GeneralBusiness)
+print("introduction to Computer Science",":", IntroductiontoComputerScience)
+print("Computer Programming",":", ComputerProgramming)
